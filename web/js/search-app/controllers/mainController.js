8e0c57d8866670325a37b23b8cc2411072d2390d
Marcus Fulbright <marcus.fulbright@iostudio.com>
2013-08-18 14:01:13 -0500

    angular wip

---
+++ web/js/search-app/controllers/mainController.js
@@ -0,0 +1,17 @@
+app.controller("mainController", function($scope, $http){
+
+    $scope.apiKey= '179b4ca157d29bca1a09a4e7e013f5f2';
+    $scope.init = function() {
+	var request = 'http://api.brewerydb.com/v2/beers/?key=' + $scope.apiKey;
+	$http({method: 'GET', url: 'request'}).
+	success(function(data, status, headers, config) {
+    // this callback will be called asynchronously
+    // when the response is available
+    }).
+	error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+    });
+	console.log(request);
+    };
+});
