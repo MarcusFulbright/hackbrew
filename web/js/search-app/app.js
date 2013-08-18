fb739632818fbc6ae2ec63b9eca0bc10acb35fb5
Marcus Fulbright <marcus.fulbright@iostudio.com>
2013-08-18 14:12:11 -0500

    fixed post type to GET

--- web/js/search-app/app.js
+++ web/js/search-app/app.js
@@ -1,1 +1,7 @@
 var app = angular.module('SearchApp',['ngResource']);
+
+app.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+]);
